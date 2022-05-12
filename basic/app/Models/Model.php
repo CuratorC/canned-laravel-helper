27ab179382d7353dbdf48dff80474d2c63415950
root <root@.localdomain>
2022-05-12 16:13:28 +0800

    update ScopeCannedSearch

--- basic/app/Models/Model.php
+++ basic/app/Models/Model.php
@@ -11,20 +11,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 /**
  * App\Models\Model
  *
- * @method static \Illuminate\Database\Eloquent\Builder|Model coderIdInPath(string $id, string $field = 'path')
- * @method static \Illuminate\Database\Eloquent\Builder|Model coderNameInPath(string $name, string $modelName, string $nameField = 'name', string $pathField = 'path')
- * @method static \Illuminate\Database\Eloquent\Builder|Model coderOrder(...$orderRules)
- * @method static \Illuminate\Database\Eloquent\Builder|Model coderPaginate()
- * @method static \Illuminate\Database\Eloquent\Builder|Model coderWhen($field, $func)
- * @method static \Illuminate\Database\Eloquent\Builder|Model coderWhenKeyword()
- * @method static \Illuminate\Database\Eloquent\Builder|Model coderWhereDate($field)
+ * @method static \Illuminate\Database\Eloquent\Builder|Model cannedIdInPath(int $id, string $field = 'path')
+ * @method static \Illuminate\Database\Eloquent\Builder|Model cannedNameInPath(string $name, string $className, string $nameField = 'name', string $pathField = 'path')
+ * @method static \Illuminate\Database\Eloquent\Builder|Model cannedOrder(...$orderRules)
+ * @method static \Illuminate\Database\Eloquent\Builder|Model cannedPaginate(bool $ableAll = true, int $defaultSize = 10)
+ * @method static \Illuminate\Database\Eloquent\Builder|Model cannedWhen(string $column, $func)
+ * @method static \Illuminate\Database\Eloquent\Builder|Model cannedWhenKeyword()
+ * @method static \Illuminate\Database\Eloquent\Builder|Model cannedWhereDate($field)
  * @method static \Illuminate\Database\Eloquent\Builder|Model newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Model newQuery()
  * @method static \Illuminate\Database\Query\Builder|Model onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|Model query()
  * @method static \Illuminate\Database\Query\Builder|Model withTrashed()
  * @method static \Illuminate\Database\Query\Builder|Model withoutTrashed()
- * @mixin \Eloquent
+ * @mixin Eloquent
  */
 class Model extends EloquentModel
 {
